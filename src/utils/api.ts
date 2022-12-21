34ecdf08cacba06da98f54b0b4e52bdd2bd42fb2
matheusfdc10 <64614285+matheusfdc10@users.noreply.github.com>
2022-12-21 00:23:55 -0300

    at

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,9 @@
 import axios from  'axios'
 
+// export const api = axios.create({
+//     baseURL: 'http://192.168.0.2:3001',
+// })
+
 export const api = axios.create({
-    baseURL: 'http://192.168.0.2:3001',
+    baseURL: 'http://192.168.1.96:3001',
 })
